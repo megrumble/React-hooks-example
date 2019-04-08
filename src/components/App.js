fe0fcea33ce9bf51ea517cbb1d071053799aef0a
unknown <megrumble@gmail.com>
2019-04-08 07:35:21 -0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class App extends React.Component {
+  state = { resource: "posts " };
+  render() {
+    return (
+      <div>
+        <div>
+          <button onClick={() => this.setState({ resource: "posts" })}>
+            Posts
+          </button>
+          <button onClick={() => this.setState({ resource: "todos" })}>
+            ToDos
+          </button>
+        </div>
+        {this.state.resource}
+      </div>
+    );
+  }
+}
+
+export default App;
