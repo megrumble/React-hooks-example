d0054fa9cdd6b9bba31ca3f625ba5fd0f4c2869b
unknown <megrumble@gmail.com>
2019-04-08 08:45:39 -0400

    refactored App component to functional component using hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
-class App extends React.Component {
-  state = { resource: "posts " };
-  render() {
-    return (
+const App = () => {
+  const [resource, setResource] = useState("posts");
+  return (
+    <div>
       <div>
-        <div>
-          <button onClick={() => this.setState({ resource: "posts" })}>
-            Posts
-          </button>
-          <button onClick={() => this.setState({ resource: "todos" })}>
-            ToDos
-          </button>
-        </div>
-        {this.state.resource}
+        <button onClick={() => setResource("posts")}>Posts</button>
+        <button onClick={() => setResource("todos")}>ToDos</button>
       </div>
-    );
-  }
-}
+      {resource}
+    </div>
+  );
+};
 
 export default App;
